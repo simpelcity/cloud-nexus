b625c2a1f0f0a3eca1c8cc1b388744e01ad57fcb
Simpelcity <wietse2007.3@gmail.com>
2025-11-25 13:37:56 +0100

    updated index.scss

--- src/main.jsx
+++ src/main.jsx
@@ -7,24 +7,24 @@ import Services from './pages/Services.jsx';
 import Pricing from './pages/Pricing.jsx';
 import About from './pages/About.jsx';
 import Contact from './pages/Contact.jsx';
-import './styles/scss/index.scss';
+import './index.scss';
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-        children: [
-            { path: '/', element: <Home /> },
-            { path: '/services', element: <Services /> },
-            { path: '/pricing', element: <Pricing /> },
-            { path: '/about-us', element: <About /> },
-            { path: '/contact', element: <Contact /> },
-        ],
-    },
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/services', element: <Services /> },
+      { path: '/pricing', element: <Pricing /> },
+      { path: '/about-us', element: <About /> },
+      { path: '/contact', element: <Contact /> },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <RouterProvider router={router} />
-    </StrictMode>,
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 )
